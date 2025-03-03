8e6d4aa5d804cebf52a7e95201e7b0a539f7651e
Emil-18 <emilhesmyr@icloud.com>
2025-03-03 18:19:51 +0100

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,28 @@
-# enhanced_language_switching
-Enhanced language switching add-on for NVDA
+# Enhanced Language Detection
+
+* Author: Emil-18
+* NVDA compatibility: 2024.2 and beyond
+* Download: [Stable version](https://github.com/Emil-18/enhanced_language_switching/releases/download/v1.0/enhancedLanguageDetection-1.0.nvda-addon).
+
+This add-on Automaticly detects the language of the text NVDA is about to speak, and uses NVDA's built in auto language switching, if turned on, to sspeak the text in that language.
+
+This add-on is heavily inspired by [the language ident add-on](https://github.com/slohmaier/LanguageIdent).
+That add-on stopped working, however, and from it's issues on GitHub, it doesn't seam like the developer is going to fix it.
+
+## Settings
+
+* Language interpretation:
+    This is a combo box that allows you to select when the add-on is interpreting the language of the text. The choices are:
+    * always interpret:
+        the add-on will always interpret the language of the text and remove all previous instructions to change the language. For example, if you are on a web page, and the language of the web page is defined as english, but according to the add-on, the text is Norwegian, it will switch to a Norwegian voice. If you haven't selected Norwegian as one of the languages to be interpreted, (see below), the voice will not change at all.
+    *only interpret if the text doesn't instruct NVDA to change language:
+        The add-on will only interpret the language if there are no instructions to change the language in the text. In the example above, it will switch to an english voice.
+    * never interpret (NVDA's default behavior):
+        Self explanatory.
+* Languages to interpret:
+    This is a list of the languages to interpret. If the add-on interprets a text as a language that isn't selected in this list, no auto language switching is done. No languages are selected by default.
+
+## Change log.
+### v1.0
+
+Initial release
---
+++ addon/globalPlugins/enhancedLanguageDetection/__init__.py
@@ -0,0 +1,111 @@
+# coding: utf-8
+# Copyright 2025 Emil-18
+# An add-on that detects languages based on the spoken text itself
+# This add-on is licensed under the same license as NVDA. See the copying.txt file for more information
+
+import addonHandler
+addonHandler.initTranslation()
+import config
+import globalPluginHandler
+import gui
+import languageHandler
+import os
+import speech
+import wx
+from scriptHandler import script
+from speech.commands import LangChangeCommand
+from speech.extensions import filter_speechSequence
+from . import langdetect
+profilePath = os.path.join(os.path.dirname(__file__), "langdetect", "profiles")
+languages = os.listdir(profilePath)
+
+confspec = {
+	"languageDetection": "integer(default=1)"
+}
+for i in os.listdir(profilePath):
+	confspec.update({i: "boolean(default=False)"})
+config.conf.spec["enhancedLanguageDetection"] = confspec
+class EnhancedLanguageDetectionSettingsPanel(gui.SettingsPanel):
+	# Translators: The title for the settings panel
+	title = _("Enhanced Language Detection")
+	def makeSettings(self, settingsSizer):
+		settings = gui.guiHelper.BoxSizerHelper(self, sizer = settingsSizer)
+		comboBoxValues = [
+			# Translators: An option in a combo box
+			_("always interpret"),
+			# Translators: An option in a combo box
+			_("only interpret if the text doesn't instruct NVDA to change language"),
+			# Translators: An option in a combo box
+			_("never interpret (NVDA's default behavior)")
+		]
+		# Translators: The label for a combo box
+		label = _("Language interpretation:")
+		self.languageDetection = settings.addLabeledControl(label, wx.Choice, choices = comboBoxValues)
+		self.languageDetection.SetSelection(config.conf["enhancedLanguageDetection"]["languageDetection"])
+		# Translators: The label for a checkable list box
+		label = _("Languages to interpret")
+		choices = []
+		for i in languages:
+			description = languageHandler.getLanguageDescription(i)
+			if description:
+				choices.append(description)
+			else:
+				choices.append(i)
+		self.languages = settings.addLabeledControl(label, gui.nvdaControls.CustomCheckListBox, choices = choices)
+		checked = []
+		for i in range(len(languages)):
+			if config.conf["enhancedLanguageDetection"][languages[i]]:
+				checked.append(i)
+		self.languages.SetCheckedItems(checked)
+	def onSave(self):
+		config.conf["enhancedLanguageDetection"]["languageDetection"] = self.languageDetection.GetSelection()
+		checked = self.languages.GetCheckedItems()
+		for i in range(len(languages)):
+			config.conf["enhancedLanguageDetection"][languages[i]] = i in checked
+allowedLanguages = ["no", "en"]
+def detectLanguage(text):
+	try:
+		languages = langdetect.detect_langs(text)
+	except:
+		languages = None
+	if not languages:
+		return
+	lang = None
+	for language in languages:
+		if config.conf["enhancedLanguageDetection"][language.lang]:
+			lang = language.lang
+			return(lang)
+def speechSequenceFilter(speechSequence, *args, **kwargs):
+	if config.conf["enhancedLanguageDetection"]["languageDetection"] == 2:
+		return(speechSequence)
+	newSequence = []
+	shouldInterpret = True
+	for i in speechSequence:
+		if  isinstance(i, LangChangeCommand) and config.conf["enhancedLanguageDetection"]["languageDetection"] == 0:
+			continue
+		if isinstance(i, LangChangeCommand):
+			shouldInterpret = not i.lang
+		if isinstance(i, str):
+			if shouldInterpret:
+				language = detectLanguage(i)
+				languageCommand = LangChangeCommand(language)
+				newSequence.append(languageCommand)
+			else:
+				newSequence.append(i)
+				continue
+
+		newSequence.append(i)
+	try:
+		language = detectLanguage(string)
+	except:
+		language = None
+	return(newSequence)
+
+class GlobalPlugin(globalPluginHandler.GlobalPlugin):
+	def __init__(self, *args, **kwargs):
+		super(GlobalPlugin, self).__init__(*args, **kwargs)
+		gui.settingsDialogs.NVDASettingsDialog.categoryClasses.append(EnhancedLanguageDetectionSettingsPanel)
+		filter_speechSequence.register(speechSequenceFilter)
+	def terminate(self, *args, **kwargs):
+		filterSpeechSequence.unRegister(speechSequenceFilter)
+		gui.settingsDialogs.NVDASettingsDialog.categoryClasses.remove
